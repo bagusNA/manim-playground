6ee53030cc77f48b2ed696fde240d2f27a0d7cbd
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2024-12-23 15:54:08 +0800

    feat: table view

--- implementations/DijkstraAlgorithm.py
+++ implementations/DijkstraAlgorithm.py
@@ -1,6 +1,10 @@
 import sys
 from typing import Dict, List, Tuple
 
+from tabulate import tabulate
+
+ANSI_COLOR_GREEN = '\033[92m'
+ANSI_COLOR_END = '\033[0m'
 
 class Graph:
     def __init__(self):
@@ -57,9 +61,14 @@ class Graph:
 class Dijkstra:
     def __init__(self, graph: Graph):
         self._graph: Graph = graph
+
         self._traveled_vertices: List[str] = []
         self._distance: Dict[str, int] = {}
         self._path: Dict[str, List[str]] = {}
+
+        # Table data
+        self._table_result: List[any] = []
+        self._printed_traveled_vertices: List[str] = []
 
     def _find_closest_untraveled_vertex(self) -> str | None:
         """
@@ -76,7 +85,50 @@ class Dijkstra:
 
         return closest_vertex
 
-    def run(self, source: str) -> None:
+    def _append_table_result(self, vertex: str) -> None:
+        self._table_result.append([
+            vertex,
+            *map(lambda item: self._format_table_result(item[0], item[1], vertex), self._distance.items()),
+        ])
+
+    def _format_table_result(self, vertex: str, distance: int, current_vertex: str) -> str:
+        if vertex == current_vertex and distance != sys.maxsize:
+            self._printed_traveled_vertices.append(vertex)
+            return f"{ANSI_COLOR_GREEN}{distance}/{vertex}{ANSI_COLOR_END}"
+
+        if vertex in self._printed_traveled_vertices:
+            return " "
+
+        return (
+            "∞" if distance == sys.maxsize else f"{distance}/{vertex}"
+        )
+
+    def _print_table(self, current_vertex) -> None:
+        table = tabulate(
+            self._table_result,
+            headers=["Traveled", *self._graph.vertices],
+            stralign="center",
+            numalign="center",
+            tablefmt="rounded_grid",
+        )
+
+        print(f"\nCurrent vertex: {current_vertex}")
+        print(table)
+
+    def _print_result(self, source: str, destination: str):
+        print(f"Shortest path from {source} using Dijkstra Algorithm:")
+
+        for vertex, result in self._path.items():
+            path = result if vertex == source else [*result, vertex]
+            formatted_path = f"{vertex}: {' -> '.join(path)} = {self._distance[vertex]}"
+
+            print(
+                formatted_path
+                if destination != vertex else
+                ANSI_COLOR_GREEN + formatted_path + ANSI_COLOR_END
+            )
+
+    def find(self, source: str, destination: str) -> None:
         """
         Isi utama implementasi algoritma dijkstra
         :param source:
@@ -99,6 +151,10 @@ class Dijkstra:
                     if closest_vertex != source:
                         self._path[vertex].append(closest_vertex)
 
+            self._append_table_result(closest_vertex)
+            self._print_table(closest_vertex)
+
+        self._print_result(source, destination)
 
 if __name__ == '__main__':
     graph = Graph()
@@ -125,9 +181,9 @@ if __name__ == '__main__':
     graph.add_edge(("V4", "V5"), 2)
 
     dijkstra = Dijkstra(graph)
-    dijkstra.run("V1")
+    dijkstra.find("V1", "V5")
 
     # print(dijkstra._graph)
     # print(dijkstra._vertices)
-    print(dijkstra._distance)
-    print(dijkstra._path)
+    # print(dijkstra._distance)
+    # print(dijkstra._path)
